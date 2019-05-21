b360853e310115868a1e598c7998a8c646c3a082
igorj <igor,jesus@hdi.hdi.br>
2019-05-20 23:10:25 -0300

    sinc prof

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -13,34 +12,33 @@ const url = 'mongodb+srv://uswjde3iz596yngjuy9r:pKpwswJcCtAGlfuZrnv0@cluster0-dn
 
 //mongostat --host Cluster0-shard-0/cluster0-shard-00-00-dnmpn.mongodb.net:27017,cluster0-shard-00-01-dnmpn.mongodb.net:27017,cluster0-shard-00-02-dnmpn.mongodb.net:27017 --ssl --username uswjde3iz596yngjuy9r --password pKpwswJcCtAGlfuZrnv0 --authenticationDatabase admin
 
-const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser:true };
+const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
 
 mongoose.connect(url, options);
 mongoose.set('useCreateIndex', true);
 
-mongoose.connection.on('error', function (err) {
+mongoose.connection.on('error', (err) => {
     console.log('Erro na conexão com o banco de dados: ' + err);
-});
+})
 
-mongoose.connection.on('disconnected', function () {
+mongoose.connection.on('disconnected', () => {
     console.log('Aplicação desconectada do banco de dados!');
-});
+})
 
-mongoose.connection.on('connected', function () {
+mongoose.connection.on('connected', () => {
     console.log('Aplicação conectada ao banco de dados!');
-});
+})
 
 //BODY PARSER
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const indexRoute = require('./Routers/index.js');
-const userRoute = require('./Routers/users.js');
+const indexRoute = require('./Routes/index');
+const usersRoute = require('./Routes/users');
 
 app.use('/', indexRoute);
-app.use('/users', userRoute);
+app.use('/users', usersRoute);
 
-//porta que a app vai ficar startada
 app.listen(3000);
 
 module.exports = app;
